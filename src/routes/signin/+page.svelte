101c9ee2e67dfd3e78bead321baea7805bc02cc0
Nakaya <eniehack@outlook.jp>
2023-02-18 21:58:11 +0900

    fix: syntax error

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -6,49 +6,48 @@
 
 <script lang="ts">
 	import { browser } from "$app/environment";
-
-    if (browser) {
-    import { ClientStorageItemSerializer } from "$lib/localstorage";
+	import { ClientStorageItemSerializer } from "$lib/localstorage";
     import { create_client, authorize_url } from "$lib/oauth";
     let instance_name: string;
+    let submit: () => Promise<void>;
 
-
-    const submit = async () => {
-        let server_url = new URL(`https://${instance_name}/`);
-        let client_url_str: string;
-        if (import.meta.env.PROD) {
-            if (import.meta.env.VITE_VERCEL === "1") {
-                client_url_str = import.meta.env.VITE_VERCEL_URL
+    if (browser) {
+        submit = async () => {
+            let server_url = new URL(`https://${instance_name}/`);
+            let client_url_str: string;
+            if (import.meta.env.PROD) {
+                if (import.meta.env.VITE_VERCEL === "1") {
+                    client_url_str = import.meta.env.VITE_VERCEL_URL
+                } else {
+                    client_url_str = import.meta.env.VITE_CLIENT_HOST
+                }
             } else {
-                client_url_str = import.meta.env.VITE_CLIENT_HOST
+                client_url_str = "localhost:5173";
             }
-        } else {
-            client_url_str = "localhost:5173";
-        }
-        console.log(client_url_str);
-        console.log(import.meta.env.VITE_CLIENT_HOST);
-        let client_url = new URL(`https://${client_url_str}`)
-        if (client_url.hostname === "localhost") {
-            client_url.protocol = "http:";
-        }
-        console.log(client_url)
-        let resp = await create_client(server_url, client_url);
-        if (resp === null) {
-            console.error("create_client: something went wrong");
-            return
-        }
+            console.log(client_url_str);
+            console.log(import.meta.env.VITE_CLIENT_HOST);
+            let client_url = new URL(`https://${client_url_str}`)
+            if (client_url.hostname === "localhost") {
+                client_url.protocol = "http:";
+            }
+            console.log(client_url)
+            let resp = await create_client(server_url, client_url);
+            if (resp === null) {
+                console.error("create_client: something went wrong");
+                return
+            }
 
-        localStorage.setItem(
-            "client",
-            ClientStorageItemSerializer({
-                client_id: resp.client_id,
-                client_secret: resp.client_secret,
-                server_url: server_url,
-                client_host: client_url,
-            })
-        );
+            localStorage.setItem(
+                "client",
+                ClientStorageItemSerializer({
+                    client_id: resp.client_id,
+                    client_secret: resp.client_secret,
+                    server_url: server_url,
+                    client_host: client_url,
+                })
+            );
 
-        location.href = authorize_url(server_url, resp.client_id, client_url).toString()
+            location.href = authorize_url(server_url, resp.client_id, client_url).toString()
+        }
     }
-}
 </script>
